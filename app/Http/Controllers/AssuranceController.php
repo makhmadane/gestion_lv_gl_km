7cb9d8a7d21ec731f13700cc306bb77c3903eed2
makhmadane96 <makhmadane.lo@jmadsolutions.sn>
2026-02-06 16:13:18 +0100

    categorie add

---
+++ app/Http/Controllers/AssuranceController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Assurance;
+use App\Models\Categorie;
+use Illuminate\Http\Request;
+
+class AssuranceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+            $assurances = Assurance::paginate(5); //model
+            return view('assurance.assurance',['assurances'=>$assurances]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Categorie::all();
+
+        return view('assurance.add',compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $assurance = new Assurance();
+        $assurance->libelle = $request['libelle'];
+        $assurance->montant = $request['montant'];
+        $assurance->category_id = $request['categorie_id'];
+        $assurance->save();
+        return to_route('assurance');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $assurance = Assurance::find($id);
+        $assurance->delete();
+    }
+}
